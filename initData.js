bd5b3bc6e5484fac019151e582b165912ec9644b
TRAN THANH TUNG <tungtran@TRANs-MacBook-Pro.local>
2020-10-09 19:06:36 +0900

    first commit

---
+++ initData.js
@@ -0,0 +1,14 @@
+[
+ {
+ "articleId": "987654",
+ "articleTitle": "Lorem ipsum - article one",
+ "articleContent": "Here goes the content of the article",
+ "price": 20
+ },
+ {
+ "articleId": "123456",
+ "articleTitle": "Lorem ipsum - article two",
+ "articleContent": "Sky is the limit, the content goes here.",
+ "price": 50
+ }
+]
---
+++ server/routes.js
@@ -0,0 +1,43 @@
+const PublishingAppRoutes = [
+  {
+    route: 'articles.length',
+    get: () => {
+      const articlesCountInDB = 2;
+      return {
+        path: ['articles', 'length'],
+        value: articlesCountInDB
+      };
+    }
+  },
+  {
+    route: 'articles[{integers}]["id","articleTitle","articleContent"]',
+    get: (pathSet) => {
+      const articlesIndex = pathSet[1];
+      const articlesArrayFromDB = [
+        {
+        'articleId': '987654',
+        'articleTitle': 'BACKEND Lorem ipsum - article one',
+        'articleContent': 'BACKEND Here goes the content of the article'
+        },
+        {
+        'articleId': '123456',
+        'articleTitle': 'BACKEND Lorem ipsum - article two',
+        'articleContent': 'BACKEND Sky is the limit, the content goes here.'
+        }
+      ]; // That are our mocked articles from MongoDB
+      let results = [];
+      articlesIndex.forEach((index) => {
+        const singleArticleObject = articlesArrayFromDB[index];
+        const falcorSingleArticleResult = {
+          path: ['articles', index],
+          value: singleArticleObject
+        };
+        results.push(falcorSingleArticleResult);
+      });
+      return results;
+    }
+  }
+];
+
+// export default PublishingAppRoutes;
+module.exports = PublishingAppRoutes;
